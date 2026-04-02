36b6c769e3bd3b293bbcda406ccc60bee40172cd
nishtha621 <nishthatandon14@gmail.com>
2026-04-02 12:02:51 +0530

    Initial Commit

---
+++ Layman/LaymanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaymanApp.swift
+//  Layman
+//
+//  Created by Nishtha Tandon on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LaymanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
